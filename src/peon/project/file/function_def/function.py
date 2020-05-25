5e016dca263c1f0f6dd6f60e5858e6048f0079f4
Eugene Gildin <roch1990@tochka.com>
2020-05-25 23:56:47 +0500

    Add tests to Function class (#16)
    * feature: add 'check_inheritence' test cases
    
    * feature: add joined string test for return expression
    
    * feature: add tests to Function class
    
    * feature: add tests to Function class

--- src/peon/project/file/function_def/function.py
+++ src/peon/project/file/function_def/function.py
@@ -1,5 +1,5 @@
 import ast
-from typing import Tuple
+from typing import Tuple, Optional, Any
 
 import _ast
 
@@ -28,7 +28,7 @@ class Function:
 
     def __init__(
             self,
-            definition: _ast.FunctionDef,
+            definition: Optional[_ast.FunctionDef],
     ):
         self.definition = definition
 
@@ -43,7 +43,7 @@ class Function:
         if not node:
             return FunctionParseResult(
                 return_not_none=bool(Function.EMPTY_RETURNED_VALUE),
-                line_number=node.lineno,
+                line_number=-1,
             )
 
         # skip plain assignment expressions
@@ -93,26 +93,27 @@ class Function:
         elif isinstance(self.definition, _ast.Expr):
             return False
 
-        # get list of method decorators
-        decorators = self.definition.decorator_list
-        # iterate over decorators
-        for item in decorators:
-            # if 'staticmethod' in decorator list
-            if item.id == 'staticmethod':
-                return True
-        # check for '_' and '__' starts chars
-        if self.definition.name.startswith('_') or self.definition.name.startswith('__'):
-            # skip magic methods
-            if not self.definition.name.endswith('__'):
-                return True
-        return False
+        try:
+            # get list of method decorators
+            decorators = self.definition.decorator_list
+            # iterate over decorators
+            for item in decorators:
+                # if 'staticmethod' in decorator list
+                if item.id == 'staticmethod':
+                    return True
+        except AttributeError:
+            pass
 
-    def name(self) -> str:
-        """
-        Strange method for fun. Yes, looks like a getter.
-        :return:
-        """
-        return self.definition.name
+        try:
+            # check for '_' and '__' starts chars
+            if self.definition.name.startswith('_') or self.definition.name.startswith('__'):
+                # skip magic methods
+                if not self.definition.name.endswith('__'):
+                    return True
+        except AttributeError:
+            raise
+
+        return False
 
     def reflection_at_line(self) -> Tuple[int]:
         """
@@ -134,7 +135,56 @@ class Function:
         # dont forget to convert to immutable type
         return tuple(reflection_list)
 
-    def constructor_non_attribs_value_line_number(self) -> Tuple[int]:
+    def constructor_non_attribs_value_line_number(self) -> Tuple[Any]:
+        """
+        Check constructor for code (non-assign expressions)
+        :return: list of line numbers, that consist not "assign" expressions
+        """
+
+        line_numbers = []
+        # skip non-functions definitions
+        if not isinstance(self.definition, _ast.FunctionDef):
+            return tuple(line_numbers)
+
+        # check only constructors
+        if self.definition.name == '__init__':
+            # iterate over constructor body
+            for expressions in self.definition.body:
+                # if not assing expression - append line number
+                if not isinstance(expressions, _ast.Assign):
+                    line_numbers.append(expressions.lineno)
+                    continue
+
+                # iterate over assign target
+                for target in expressions.targets:
+                    # if assign target is not attribute - append line number
+                    if not isinstance(target, _ast.Attribute):
+                        line_numbers.append(target.lineno)
+
+        return tuple(line_numbers)
+
+    def non_assert_methods_at_test_function(self) -> Tuple[Any]:
+        """
+        Check, that test method consist of only assert expressions
+        :return: tuple of line-numbers, where found non-assert expressions
+        """
+
+        line_numbers = []
+        if not isinstance(self.definition, _ast.FunctionDef):
+            return tuple(line_numbers)
+
+        if self.definition.name.startswith('test'):
+            for expressions in self.definition.body:
+                if not isinstance(expressions, _ast.Assert):
+                    line_numbers.append(expressions.lineno)
+
+        return tuple(line_numbers)
+
+    def constructor_mutable_attribs_line_number(self) -> Tuple[Any]:
+        """
+        Check for mutable class attribs at constructor
+        :return: tuple of line numbers, where was found mutable attribs
+        """
 
         line_numbers = []
         if not isinstance(self.definition, _ast.FunctionDef):
@@ -142,58 +192,36 @@ class Function:
 
         if self.definition.name == '__init__':
             for expressions in self.definition.body:
-                if not isinstance(expressions, _ast.Assign):
-                    line_numbers.append(expressions.lineno)
+                # list
+                try:
+                    value = expressions.value
+                    if isinstance(value, _ast.List):
+                        line_numbers.append(expressions.lineno)
+                    elif isinstance(value, _ast.Set):
+                        line_numbers.append(expressions.lineno)
+                    elif isinstance(value, _ast.Dict):
+                        line_numbers.append(expressions.lineno)
+                    elif value.__dict__.get('func') is not None:
+                        if value \
+                                .__dict__.get('func') \
+                                .__dict__.get('id') \
+                                in self.MUTABLE_TYPES:
+                            line_numbers.append(expressions.lineno)
+                except AttributeError:
                     continue
-
-                for target in expressions.targets:
-                    if not isinstance(target, _ast.Attribute):
-                        line_numbers.append(target.lineno)
 
         return tuple(line_numbers)
 
-    def non_assert_methods_at_test_function(self):
+    def set_encapsulated_attribs_line_numbers(self) -> Tuple[Any]:
+        """
+        Check for setters expressions into class methods
+        For example, trigger at this point outside constructor: `self.some = some_var`
+        :return: tuple of line numbers, where was found mutable attribs
+        """
 
         line_numbers = []
         if not isinstance(self.definition, _ast.FunctionDef):
-            return line_numbers
-
-        if self.definition.name.startswith('test'):
-            for expressions in self.definition.body:
-                if not isinstance(expressions, _ast.Assert):
-                    line_numbers.append(expressions.lineno)
-
-        return line_numbers
-
-    def constructor_mutable_attribs_line_number(self):
-
-        line_numbers = []
-        if not isinstance(self.definition, _ast.FunctionDef):
-            return line_numbers
-
-        if self.definition.name == '__init__':
-            for expressions in self.definition.body:
-                # list
-                if expressions.value.__dict__.get('elts') is not None:
-                    line_numbers.append(expressions.lineno)
-                # dict
-                elif expressions.value.__dict__.get('keys') is not None:
-                    line_numbers.append(expressions.lineno)
-                # via keywords (list(), dict(), set())
-                elif expressions.value.__dict__.get('func') is not None:
-                    if expressions.value \
-                            .__dict__.get('func') \
-                            .__dict__.get('id') \
-                            in self.MUTABLE_TYPES:
-                        line_numbers.append(expressions.lineno)
-
-        return line_numbers
-
-    def set_encapsulated_attribs_line_numbers(self):
-
-        line_numbers = []
-        if not isinstance(self.definition, _ast.FunctionDef):
-            return line_numbers
+            return tuple(line_numbers)
 
         if self.definition.name != '__init__':
 
@@ -202,19 +230,26 @@ class Function:
                     for target in expressions.targets:
                         # check for keyword 'self'
                         try:
-                            value = target.value.value.id
+                            value = target.value.id
                             if value == 'self':
                                 line_numbers.append(target.lineno)
                         except AttributeError:
                             continue
-        return line_numbers
+        return tuple(line_numbers)
 
-    def setter_or_getters_def_names_line_numbers(self):
+    def setter_or_getters_def_names_line_numbers(self) -> Tuple[Any]:
+        """
+        Check method name for 'get' or 'set' keywords at method name start.
+        :return: tuple of line numbers, where was found this keywords at name start
+        """
 
         line_numbers = []
         if not isinstance(self.definition, _ast.FunctionDef):
-            return line_numbers
+            return tuple(line_numbers)
+
+        if not self.definition.name:
+            return tuple(line_numbers)
 
         if self.definition.name.startswith('set') or self.definition.name.startswith('get'):
             line_numbers.append(self.definition.lineno)
-        return line_numbers
+        return tuple(line_numbers)
