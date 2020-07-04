a6a8615a3299185839d590c4a89f9066d4cb2383
roch1990 <roch1990@tochka.com>
2020-07-04 23:25:50 +0500

    feature: additional cmd flags

--- peon/tests/test_lint/test_lint.py
+++ peon/tests/test_lint/test_lint.py
@@ -6,13 +6,17 @@ from peon.src.lint.report.report_channels import ReportChannels
 
 
 def test_sucess():
-    assert Lint(TestProjectTree().project_tree.inspect(), output_channel=ReportChannels.stdout).project() is None
+    assert Lint(
+        TestProjectTree().project_tree.inspect(),
+        output_channel=ReportChannels.stdout,
+        result_independence=True,
+    ).project() is None
 
 
 def test_none_path():
     with pytest.raises(TypeError):
-        assert Lint(None, output_channel=ReportChannels.stdout).project()
+        assert Lint(None, output_channel=ReportChannels.stdout, result_independence=True).project()
 
 
 def test_empty_project():
-    assert Lint([], output_channel=ReportChannels.stdout).project() is None
+    assert Lint([], output_channel=ReportChannels.stdout, result_independence=True).project() is None
